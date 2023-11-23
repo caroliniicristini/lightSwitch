2e69045dd6f6de783ead5c00c4c2009c5a794537
caroliniicristini <carolinicristini1@gmail.com>
2023-11-23 16:54:52 -0300

    lampada on/off

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("click", function(){
+
+    const lampada = document.getElementById("space_lamp");
+
+    if(lampada.id === 'space_lamp'){
+        lampada.classList.toggle("on");
+    }
+    //toggle para verificação :D
+})
